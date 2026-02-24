f6aa939c03331089b04c2b3ddeb7850bf3df783f
Dav10-32 <david.palacios-p@mail.escuelaing.edu.co>
2026-02-23 23:44:54 -0500

    feat: Estructura inicial

---
+++ src/main/java/edu/arsw/AlphaVantageApplication.java
@@ -0,0 +1,13 @@
+package edu.arsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaVantageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaVantageApplication.class, args);
+	}
+
+}
